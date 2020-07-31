4bf519d422e2c25433617f3a5613bb6833bdafaa
Fluke <jesadakorn.kirtnu@gmail.com>
2020-08-01 02:49:11 +0700

    success create mutation query and return value

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
 const express = require('express')
+const app = express()
 const { ApolloServer, gql } = require('apollo-server-express')
-const { resolvers, typeDefs } = require('./schema/index')
+// const { resolvers, typeDefs } = require('./schema/index')
 const mongooose = require('mongoose')
 require('dotenv').config
+const User = require('./models/user')
+
 const DB_USERNAME = process.env.DB_USERNAME
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_NAME = process.env.DB_NAME
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 7000
 
 const createServer = async () => {
     try {
-        await mongooose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@portfolio.4a0sq.gcp.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, { useUnifiedTopology: true },)
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers
-    })
+        await mongooose.connect(`mongodb://localhost:27017/test`, { useNewUrlParser: true, useUnifiedTopology: true })
+            .then((e) => console.log(`MongoDB connected`))
 
-    const app = express()
+        const resolvers = {
+            Query: {
+                me: (_parents, _args, _context, _info) => me,
+                user: (_parents, args, _context, _info) => {
+                    const id = args.id
 
-    server.applyMiddleware({ app })
+                    const user = users.find(u => u.id === id)
+                    return user
+                },
+                users: (_parents, _args, _context, _info) => users
+            },
+            Mutation: {
+                signup: (_parents, args, _context, _info) => {
+                    return User.create(args)
+                }
+            }
+        }
 
-    app.listen(PORT, () => console.log(`
+        const typeDefs = gql`
+            type Query {
+                me: User!
+                user(id: ID!): User
+                users: [User]!
+            }
+          
+            type Mutation {
+                signup(name: String!, email: String!, password: String!): User
+            }
+          
+            type User {
+                id: ID!
+                name: String!
+                email: String!
+                password: String!
+            }
+        `
+
+        const server = new ApolloServer({
+            typeDefs,
+            resolvers
+        })
+
+        server.applyMiddleware({ app })
+
+        app.listen({ port: PORT }, () => console.log(`
             Server running on localhost : ${PORT} ${server.graphqlPath}
         `))
-} catch (e) {
-    console.error(e)
-}
+    } catch (e) {
+        console.error(e)
+    }
 }
 
 createServer();
--- schema/index.js
+++ schema/index.js
@@ -1,28 +1,22 @@
-const { gql } = require('apollo-server-express')
+// const { gql } = require('apollo-server-express')
 
-const resolvers = {
-    Query: {
-        me: (_parents, context, args, _info) => {
-            return;
-        }
-    }
-}
-const typeDefs = gql`
-    type Query {
-        me: User!
-        users: [User]!
-        user(id:ID!): User!
-    }
-    type User {
-        id:ID!
-        name:String!
-    }
-`
+// const resolvers = {
+//     Query: {
+//         me: (_parents, context, args, _info) => {
+//             return "hello"
+//         }
+//     }
+// }
+// const typeDefs = gql`
+//     type Query {
+//         me: User!
+//         users: [User]!
+//         user(id:ID!): User!
+//     }
+//     type User {
+//         id:ID!
+//         name:String!
+//     }
+// `
 
-const Mutation = {
-    Query: () => {
-        return "hello"
-    }
-}
-
-module.exports = { Mutation, typeDefs, resolvers }
+// module.exports = { Mutation, typeDefs, resolvers }
